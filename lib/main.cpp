70e39c5d425dd4755d039bf177c22ba5d841830f
brianlong <blong8334@gmail.com>
2020-04-25 09:09:21 -0400

    got rid of some crap

--- lib/main.cpp
+++ lib/main.cpp
@@ -1,17 +1,76 @@
 #include <iostream>
+#include <string>
 
-int add(int, int);
-int bobby();
-// int add(int, int, char);
+long long int power(int base, int exponent);
 
-int add(int x, int y, char s)
+void byteTester()
 {
-  return x + y;
+  short int shortInt;
+  std::cout << sizeof(short int) << '\n';
+  std::cout << sizeof(int) << '\n';
+  std::cout << sizeof(long int) << '\n';
+  std::cout << sizeof(long long int) << '\n';
+}
+
+long long int getMax(int bytes)
+{
+  unsigned long long int total = 0;
+  for (short int i = 0; i < bytes * 8; i++)
+  {
+    unsigned long long int result = power(2, i);
+    std::cout << "power result: " << result << '\n';
+    total += result;
+    std::cout << "total so far: " << total << '\n';
+  }
+  return total;
+}
+
+long long int power(int base, int exponent)
+{
+  unsigned long long int total = 1;
+  for (int i = 1; i <= exponent; i++)
+  {
+    total *= base;
+  }
+  return total;
+}
+
+void print(int val)
+{
+  std::cout << "val: " << val << '\n';
+  std::cout << "location: " << &val << "\n\n";
 }
 
 int main()
 {
-  std::cout << add(1, 2) << '\n';
+  int x = 5;
+  int *y = &x;
+  std::cout << "x: " << x << '\n';
+  std::cout << "address: " << &x << "\n";
+  std::cout << "y: " << y << '\n';
+  std::cout << "address: " << &y << "\n";
+  std::cout << "value: " << *y << "\n\n";
+
+  x = 4;
+  std::cout << "x: " << x << '\n';
+  std::cout << "address: " << &x << "\n";
+  std::cout << "y: " << y << '\n';
+  std::cout << "address: " << &y << "\n";
+  std::cout << "value: " << *y << "\n\n";
+
+  // std::cout << z << '\n';
+  // std::cout << &z << "\n\n";
+
+  // x = 4;
+
+  // std::cout << x << '\n';
+  // std::cout << &x << "\n\n";
+
+  // std::cout << *y << '\n';
+  // std::cout << &y << "\n\n";
+
+  // std::cout << *z << '\n';
+  // std::cout << &z << "\n\n";
 
   return 0;
 }
--- notes.md
+++ notes.md
@@ -3,3 +3,36 @@
    1. ``int num1 = 10;``
    2. ``int num2(10);``
    3. ``int num3{20}; // Preferred``
+2. Pointers
+   1. ```c++
+      int x = 5;
+      int &y = x;
+      std::cout << "x: " << x << '\n';
+      std::cout << "address: " << &x << "\n\n";
+      std::cout << "y: " << y << '\n';
+      std::cout << "address: " << &y << "\n\n";
+      -----OUTPUT--------
+      x: 5
+      address: 0x7ffeea935b78
+      y: 5
+      address: 0x7ffeea935b78
+      ```
+      1. y is referencing the same memory location as x. it is essentially an alias for x.
+      2. & is the "address of" operator.  
+   2. ```c++
+      int x = 5;
+      int *y = &x;
+      std::cout << "x: " << x << '\n';
+      std::cout << "address: " << &x << "\n\n";
+      std::cout << "y: " << y << '\n';
+      std::cout << "address: " << &y << "\n";
+      std::cout << "value: " << *y << "\n\n";
+      ------OUTPUT---------
+      x: 5
+      address: 0x7ffeedefeb78
+      y: 0x7ffeedefeb78
+      address: 0x7ffeedefeb70
+      value: 5
+      ```
+      1. y points to x's spot in memory. BUT y gets its own spot in memory.
+      2. * is the "dereference" operator. It allows us to access the value at a particular address. 
